c74d975bf7eebd9a174fd6db0296ce2138c8321e
Niels <7136117+TheDutchDev@users.noreply.github.com>
2023-12-03 10:27:20 +0100

    add missing xdocs

--- PedConstants.cs
+++ PedConstants.cs
@@ -1,6 +1,12 @@
 ﻿namespace AltV.Atlas.Peds.Shared;
 
+/// <summary>
+/// Contains constants for ped module
+/// </summary>
 public static class PedConstants
 {
+    /// <summary>
+    /// The "CurrentTask" meta key that is used to set stream synced meta for peds
+    /// </summary>
     public const string CurrentTaskMetaKey = "atlas:peds:currentTask";
 }
